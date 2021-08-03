908ccf63c42fc7c84c88c71d09f03cbe7013e609
JFmaia <jfmaia741@gmail.com>
2021-08-03 17:47:35 -0300

    modified file #42
    Adicionando os templates nas configurações.

--- bsin/bsin/settings.py
+++ bsin/bsin/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'bsin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

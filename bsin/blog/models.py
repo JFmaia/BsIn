8719ab421cbe86af199f5fa20702f257e1511c24
JFmaia <jfmaia741@gmail.com>
2021-08-03 16:35:21 -0300

    modified file
    Criando modelo para o post.

--- bsin/blog/models.py
+++ bsin/blog/models.py
@@ -1,3 +1,20 @@
+from django import db
 from django.db import models
+from django.contrib.auth.models import User
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    descrição = models.TextField()
+    date = models.DateTimeField( auto_now_add= True)
+    foto = models.ImageField()
+    autor = models.ForeignKey(User,on_delete=models.CASCADE)
+    
 
-# Create your models here.
+    def __init__(self: models, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+    
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        db_table = 'post'
+    

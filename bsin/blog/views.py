ebb4099cbfdca416f89841410a89402f68a0729f
JFmaia <jfmaia741@gmail.com>
2021-08-04 12:20:03 -0300

    modified file #46
    Criando view para o login.

--- bsin/blog/views.py
+++ bsin/blog/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth import authenticate, login
+from django.contrib.messages import get_messages
 # Create your views here.
 
 def login_user(request):
     return render(request, 'login.html')
+
+@csrf_protect
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('/login/#')
+        else:
+            get_messages("Usuário ou senha inválido! Tente novamente.")
+    return redirect('/login/')
+

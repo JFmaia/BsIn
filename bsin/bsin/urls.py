181ec7a14ce3733234911558dbfd5fb0cd2e5061
JFmaia <jfmaia741@gmail.com>
2021-08-04 12:19:39 -0300

    modified file #46
    Criando url para o login.

--- bsin/bsin/urls.py
+++ bsin/bsin/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.login_user)
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
 ]

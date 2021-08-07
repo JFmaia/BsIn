1f251919f82571d846fca88bb507d7bb69415651
JFmaia <jfmaia741@gmail.com>
2021-08-07 18:14:03 -0300

    modified file #42
    Criando views para cadastrar usuario e checar o email.

--- bsin/blog/views.py
+++ bsin/blog/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from .forms import CadastroUsuarioForm
 
 # Create your views here.
 
@@ -25,7 +26,39 @@ def submit_login(request):
     return redirect('/login/')
 
 
+def cadastrar_user(request):
+    if request.method == "POST":
+        form_usuario = CadastroUsuarioForm(request.POST)
+        if form_usuario.is_valid():
+            try:
+                criar_usuario(request, form_usuario)
+                messages.success(request, 'Usuário cadastrado com sucesso.')
+                return redirect('login.html')
+            except ValidationError as e:
+                form_usuario.add_error(None, e)
+        else:
+            messages.error(request, 'O formulário contém dados inválidos!')
+    else:
+        form_usuario = CadastroUsuarioForm()
+    return render(request, 'cadastro.html', {'form_usuario': form_usuario})
 
+
+def check_email(request, form_usuario):
+    email = form_usuario.cleaned_data.get('email')
+    if User.objects.filter(email=email).exists():
+        msg = 'O e-mail informado já foi cadastrado!'
+        messages.error(request, msg)
+        form_usuario.add_error('email', msg)
+        raise ValidationError("Email já existe!")
+
+def criar_usuario(request, form_usuario):
+    usuario = form_usuario.save(commit=False)
+
+    # check_username(request, form_usuario)
+
+    check_email(request, form_usuario)
+
+    usuario.save()
 
    
 

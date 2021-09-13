9c2aede2080afb9122fe3e033a77f758d293ae04
JFmaia <jfmaia741@gmail.com>
2021-09-13 08:15:19 -0300

    Modified File
    Mudando os help_text para ter mais sentido.

--- bsin/blog/forms.py
+++ bsin/blog/forms.py
@@ -10,11 +10,11 @@ class CommentForm(forms.ModelForm):
 
 class CadastroUsuarioForm(UserCreationForm):
     username = forms.CharField(max_length=30, required=True,
-                            help_text='Obrigatório. 30 caracteres ou menos. '
+                            help_text='Obrigatório. O maximo é 30 caracteris. '
                                         'Letras minúsculas, números e @ . + - _ apenas.')
     email = forms.EmailField(max_length=254, required=True, help_text='Obrigatório. Informe um e-mail válido.')
 
-    foto = forms.ImageField(required=False, help_text='Obrigatório. Informe um e-mail válido.')
+    foto = forms.ImageField(required=False, help_text='A foto é opcional')
 
     class Meta:
         model = User

5bcd272da1e6c34c96cb183f48c973af270222b2
JFmaia <jfmaia741@gmail.com>
2021-09-13 08:15:56 -0300

    Modified File
    Craindo dois tests, para verificar se o user form é valido ou não.

--- bsin/blog/tests.py
+++ bsin/blog/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from blog.forms import CadastroUsuarioForm
 
-# Create your tests here.
+class UserViewTests(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(UserViewTests, cls).setUpClass()
+        print('\nUserViewTests')
+        
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+
+    def test_UserForm_valid(self):
+        form = CadastroUsuarioForm(data={'username': "joseflavio",
+                                        'password1': "josepassword",
+                                        'password2': "josepassword",
+                                        'email': "jose@gmail.com",
+                                        'foto': ""})
+        self.assertTrue(form.is_valid())
+
+
+    def test_UserForm_invalid(self):
+        form = CadastroUsuarioForm(data={'username': "joseflavio",
+                                        'password1': "josepassword",
+                                        'password2': "josepassword",
+                                        'email': "john@thebest.com",
+                                        'foto': ""})
+        self.assertTrue(form.is_valid())
+
+
